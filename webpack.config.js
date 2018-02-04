1faa2f7cef3295bfefc4fd60734d1d8ee195b5b3
ismail <ismail@localhost.localdomain>
2018-02-04 16:06:53 +0200

    make a file structure #6

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+module.exports = {
+    "entry": "./src/frontEnd/index.js",
+    "output": {
+        "filename": "bundle.js",
+        "path": path.resolve("public/")
+    },
+    "resolve": {
+        "extensions": [
+            ".js",
+            ".jsx"
+        ]
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "use": "babel-loader"
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                "test": /\.(png|jpg|gif)$/,
+                "use": [
+                    {
+                        "loader": "file-loader",
+                        "options": {
+                            "name": "[path][name].[ext]",
+                            "context": path.resolve("src/frontEnd/assets/")
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    "devtool": "source-map",
+    "devServer": {"historyApiFallback": true},
+    "watch": true
+};
