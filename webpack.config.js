5f85e9d2db2c853bab106b10a2ba89fce249eddd
ismail <ismail@localhost.localdomain>
2018-02-05 16:24:25 +0200

    fix elsint problem and add container in html change entry path to public in webpack-config #6

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    "entry": "./src/client/index.js",
-    "output": {
-        "filename": "bundle.js",
-        "path": path.resolve("public/")
-    },
-    "resolve": {
-        "extensions": [
-            ".js",
-            ".jsx"
-        ]
-    },
-    "module": {
-        "rules": [
-            {
-                "test": /\.js$/,
-                "exclude": /node_modules/,
-                "use": "babel-loader"
+  entry: './src/client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('public/'),
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: path.resolve('src/client/assets/'),
             },
-            {
-                "test": /\.css$/,
-                "use": [
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                "test": /\.(png|jpg|gif)$/,
-                "use": [
-                    {
-                        "loader": "file-loader",
-                        "options": {
-                            "name": "[path][name].[ext]",
-                            "context": path.resolve("src/client/assets/")
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    "devtool": "source-map",
-    "devServer": {"historyApiFallback": true},
-    "watch": true
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: { historyApiFallback: true },
+  watch: true,
 };
